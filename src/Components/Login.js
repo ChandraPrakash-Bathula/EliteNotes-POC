1105f0231dc4b74f7cfa1b01ecf7ac4ca8572327
Chandra Prakash Bathula <chandraprakashbathula@Chandras-MacBook-Pro.local>
2024-07-13 13:49:50 -0500

    added one more input field

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Cover from "../utils/Cover.webp";
+import { useState } from "react";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -10,7 +15,16 @@ const Login = () => {
         <img src={Cover} alt="background-image" className="w-screen h-1/2" />
       </div>
       <form className="absolute my-36 mx-auto p-12 text-white bg-black rounded-lg w-3/12 right-0 left-0 bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-md"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
@@ -22,9 +36,13 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700 rounded-md"
         />
         <button className="p-4 my-6 bg-red-600 w-full rounded-md">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className='py-6'>New to EliteNotes? Sign Up Now</p>
+        <p className="py-6 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to EliteNotes? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
