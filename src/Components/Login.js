22cde7f69af2d2a7289e64266fec05df1ad42d4a
Chandra Prakash Bathula <chandraprakashbathula@Chandras-MacBook-Pro.local>
2024-07-13 13:35:48 -0500

    added a paragraph

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,9 +11,20 @@ const Login = () => {
       </div>
       <form className="absolute my-36 mx-auto p-12 text-white bg-black rounded-lg w-3/12 right-0 left-0 bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">Sign In</h1>
-        <input type='text' placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700 rounded-md" />
-        <input type='password' placeholder="Password" className="p-4 my-4 w-full bg-gray-700 rounded-md" />
-        <button className="p-4 my-6 bg-red-600 w-full rounded-md">Sign In</button>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <button className="p-4 my-6 bg-red-600 w-full rounded-md">
+          Sign In
+        </button>
+        <p className='py-6'>New to EliteNotes? Sign Up Now</p>
       </form>
     </div>
   );
