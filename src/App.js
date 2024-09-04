8cff53304e53a21601aff6d3cbe5a74e39705fb7
Chandra Prakash Bathula <chandraprakashbathula@Chandras-MBP.lan>
2024-09-03 23:34:44 -0500

    added insights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import Body from "./Components/Body";
 import appStore from "./utils/appStore";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Provider store={appStore}>
     <Body />
+    <Analytics />
     </Provider>
   )
 }
